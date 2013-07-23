002ab6f45ab5ded49daf43a37dbdf4e0d07077e4
Aaron Culich <aculich@eecs.berkeley.edu>
2013-07-23 19:28:09 +0000

    copied directly from jason's test.py code so that we can begin refactoring using non-subprocess (vagrant module specific code)

--- start-inside.py
+++ start-inside.py
@@ -16,7 +16,26 @@ def initialize(opts, args):
     return config
 
 def run(config):
-    debug('something')
+    v = vagrant.Vagrant()
+    info("Initializing Vagrant cell...")
+    v.init("raring64")
+    info("Modifying Vagrantfile...")
+    replaceIf("# config.vm.network :pri", "config.vm.network :pri", provider == "kvm")
+    info("Booting up cell...")
+    v.up(provider)
+    info("Finalizing new cell...")
+    if provider != "kvm":
+       env.host_string = v.user_hostname()
+       env.key_filename = v.keyfile()
+       env.disable_known_hosts = True
+    shell_command = "vagrant status | grep 'running (' | awk '{ print $2$3 }'"
+    event = Popen(shell_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = event.communicate()
+    if output[0].find("running") != -1:
+       prvdr = output[0][8:len(output[0])-2]
+       info("Successfully created new " + prvdr + " cell!")
+    else: warn("Something went horribly wrong.")
+    print()
 
 def cleanup(config):
     pass
