009a5f3ef7673057aaca8efdc7e230895e681b00
Jason McGhee <mcghee.j@berkeley.edu>
2013-08-01 14:05:20 -0700

    I'm bad at git.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class test {
 
 	file { '/home/vagrant/readme.txt':
 		ensure => present,
-		content => "Sylvan is evil. Hello, World! Test complete. -Robot Aaron",
+		content => "Sylvan is evil, and so am I, apparently. Hello, World! Test complete. -Robot Aaron",
 	}
 }	
 
